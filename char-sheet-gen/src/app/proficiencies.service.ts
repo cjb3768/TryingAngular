d358e15554c99b387d9ed9602cd95801f2bb791d
Cullen Brown <cjb3768@gmail.com>
2019-04-24 17:42:41 -0500

    Added rudimentary error checking to ProficiencyService's add functions

--- char-sheet-gen/src/app/proficiencies.service.ts
+++ char-sheet-gen/src/app/proficiencies.service.ts
@@ -33,41 +33,48 @@ export class ProficienciesService {
     return (this.expertises.hasOwnProperty(expertiseType) && this.expertises[expertiseType].includes(expertiseName));
   }
 
-  addProficiency(proficiencyType: string, proficiencyName: string){
+  addProficiency(proficiencyType: string, proficiencyName: string): string{
     //verify that the proficiency we want to add is valid
     if (this.proficiencies.hasOwnProperty(proficiencyType)){
       //verify that the character does not already have that proficiency
-      if (!this.proficiencies[proficiencyType].indexOf(proficiencyName)){
+      if (!this.proficiencies[proficiencyType].includes(proficiencyName)){
         this.proficiencies[proficiencyType].push(proficiencyName);
+        return `Proficiency "${proficiencyName}" added`;
       }
       else{
         //no need to add the proficiency; it's already there
+        return `Error: Proficiency "${proficiencyName}" already exists`;
       }
     }
     else{
       //invalid proficiency type
+      return `Error: Invalid proficiency type`;
     }
   }
 
-  addExpertise(expertiseType: string, expertiseName: string){
+  addExpertise(expertiseType: string, expertiseName: string): string{
     //verify that the expertise type is valid (skills or tools)
     if (this.expertises.hasOwnProperty(expertiseType)){
       //verify that the character already has proficiency in the expertise type
-      if (this.proficiencies[expertiseType].indexOf(expertiseName)){
+      if (this.proficiencies[expertiseType].includes(expertiseName)){
         //verify the character doesn't already have expertise in the skill
-        if (!this.expertises[expertiseType.indexOf(expertiseName)]){
+        if (!this.expertises[expertiseType].includes(expertiseName)){
           this.expertises[expertiseType].push(expertiseName);
+          return `Expertise "${expertiseName}" added`;
         }
         else{
           //character already has expertise with the appropriate skill or tool
+          return `Error: Expertise "${expertiseName}" already exists`;
         }
       }
       else{
         //character doesn't have proficiency in the appropriate skill or tool
+        return `Error: Lacking proficiency in "${expertiseName}"`;
       }
     }
     else{
       //invalid expertise type
+      return `Error: Invalid expertise type`;
     }
   }
 
