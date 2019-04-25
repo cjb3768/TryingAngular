0c4405934b5bd7f3821c02ec9b8b5abffa194b56
Cullen Brown <cjb3768@gmail.com>
2019-04-25 16:19:46 -0500

    Created SkillControlList class for manual skill proficiency/expertise editing

---
+++ char-sheet-gen/src/app/skill-control-list.ts
@@ -0,0 +1,22 @@
+import { FormControl } from '@angular/forms';
+
+export class SkillControlList{
+  acrobatics: FormControl;
+  animalHandling: FormControl;
+  arcana: FormControl;
+  athletics: FormControl;
+  deception: FormControl;
+  history: FormControl;
+  insight: FormControl;
+  intimidation: FormControl;
+  investigation: FormControl;
+  medicine: FormControl;
+  nature: FormControl;
+  perception: FormControl;
+  performance: FormControl;
+  persuasion: FormControl;
+  religion: FormControl;
+  sleightOfHand: FormControl;
+  stealth: FormControl;
+  survival: FormControl;
+}
