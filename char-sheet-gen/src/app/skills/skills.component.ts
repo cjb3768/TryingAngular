8a9542ee973e1952322054ac54a69d18b29ec555
Cullen Brown <cjb3768@gmail.com>
2019-04-25 18:29:48 -0500

    Linting

--- char-sheet-gen/src/app/skills/skills.component.ts
+++ char-sheet-gen/src/app/skills/skills.component.ts
@@ -14,24 +14,24 @@ import { SkillControlList } from '../skill-control-list';
 export class SkillsComponent implements OnInit {
 
   skills: SkillList = {
-    acrobatics:     {name: 'acrobatics', printName: 'Acrobatics', ability: 'str', modifier: this.calculateSkillModifier('acrobatics','str')},
+    acrobatics:     {name: 'acrobatics',     printName: 'Acrobatics',      ability: 'str', modifier: this.calculateSkillModifier('acrobatics','str')},
     animalHandling: {name: 'animalHandling', printName: 'Animal Handling', ability: 'wis', modifier: this.calculateSkillModifier('animalHandling', 'wis')},
-    arcana:         {name: 'arcana', printName: 'Arcana', ability: 'int', modifier: this.calculateSkillModifier('arcana','int')},
-    athletics:      {name: 'athletics', printName: 'Athletics', ability: 'str', modifier: this.calculateSkillModifier('athletics','str')},
-    deception:      {name: 'deception', printName: 'Deception', ability: 'cha', modifier: this.calculateSkillModifier('deception','cha')},
-    history:        {name: 'history', printName: 'History', ability: 'int', modifier: this.calculateSkillModifier('history','int')},
-    insight:        {name: 'insight', printName: 'Insight', ability: 'wis', modifier: this.calculateSkillModifier('insight','wis')},
-    intimidation:   {name: 'intimidation', printName: 'Intimidation', ability: 'cha', modifier: this.calculateSkillModifier('intimidation','cha')},
-    investigation:  {name: 'investigation', printName: 'Investigation', ability: 'int', modifier: this.calculateSkillModifier('investigation','int')},
-    medicine:       {name: 'medicine', printName: 'Medicine', ability: 'wis', modifier: this.calculateSkillModifier('medicine','wis')},
-    nature:         {name: 'nature', printName: 'Nature', ability: 'int', modifier: this.calculateSkillModifier('nature','int')},
-    perception:     {name: 'perception', printName: 'Perception', ability: 'wis', modifier: this.calculateSkillModifier('perception','wis')},
-    performance:    {name: 'performance', printName: 'Performance', ability: 'cha', modifier: this.calculateSkillModifier('performance','cha')},
-    persuasion:     {name: 'persuasion', printName: 'Persuasion', ability: 'cha', modifier: this.calculateSkillModifier('persuasion','cha')},
-    religion:       {name: 'religion', printName: 'Religion', ability: 'int', modifier: this.calculateSkillModifier('religion','int')},
-    sleightOfHand:  {name: 'sleightOfHand', printName: 'Sleight of Hand', ability: 'dex', modifier: this.calculateSkillModifier('sleightOfHand','dex')},
-    stealth:        {name: 'stealth', printName: 'Stealth', ability: 'dex', modifier: this.calculateSkillModifier('stealth','dex')},
-    survival:       {name: 'survival', printName: 'Survival', ability: 'wis', modifier: this.calculateSkillModifier('survival','wis')}
+    arcana:         {name: 'arcana',         printName: 'Arcana',          ability: 'int', modifier: this.calculateSkillModifier('arcana','int')},
+    athletics:      {name: 'athletics',      printName: 'Athletics',       ability: 'str', modifier: this.calculateSkillModifier('athletics','str')},
+    deception:      {name: 'deception',      printName: 'Deception',       ability: 'cha', modifier: this.calculateSkillModifier('deception','cha')},
+    history:        {name: 'history',        printName: 'History',         ability: 'int', modifier: this.calculateSkillModifier('history','int')},
+    insight:        {name: 'insight',        printName: 'Insight',         ability: 'wis', modifier: this.calculateSkillModifier('insight','wis')},
+    intimidation:   {name: 'intimidation',   printName: 'Intimidation',    ability: 'cha', modifier: this.calculateSkillModifier('intimidation','cha')},
+    investigation:  {name: 'investigation',  printName: 'Investigation',   ability: 'int', modifier: this.calculateSkillModifier('investigation','int')},
+    medicine:       {name: 'medicine',       printName: 'Medicine',        ability: 'wis', modifier: this.calculateSkillModifier('medicine','wis')},
+    nature:         {name: 'nature',         printName: 'Nature',          ability: 'int', modifier: this.calculateSkillModifier('nature','int')},
+    perception:     {name: 'perception',     printName: 'Perception',      ability: 'wis', modifier: this.calculateSkillModifier('perception','wis')},
+    performance:    {name: 'performance',    printName: 'Performance',     ability: 'cha', modifier: this.calculateSkillModifier('performance','cha')},
+    persuasion:     {name: 'persuasion',     printName: 'Persuasion',      ability: 'cha', modifier: this.calculateSkillModifier('persuasion','cha')},
+    religion:       {name: 'religion',       printName: 'Religion',        ability: 'int', modifier: this.calculateSkillModifier('religion','int')},
+    sleightOfHand:  {name: 'sleightOfHand',  printName: 'Sleight of Hand', ability: 'dex', modifier: this.calculateSkillModifier('sleightOfHand','dex')},
+    stealth:        {name: 'stealth',        printName: 'Stealth',         ability: 'dex', modifier: this.calculateSkillModifier('stealth','dex')},
+    survival:       {name: 'survival',       printName: 'Survival',        ability: 'wis', modifier: this.calculateSkillModifier('survival','wis')}
   };
 
   //form controls
