f63d40c6cbd6d885fa2c100f1e8ed1b3a3393ea3
Cullen Brown <cjb3768@gmail.com>
2019-04-25 13:50:57 -0500

    Created Skills Component

--- char-sheet-gen/src/app/app.module.ts
+++ char-sheet-gen/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AbilityScoresComponent } from './ability-scores/ability-scores.component';
 import { ProficienciesComponent } from './proficiencies/proficiencies.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AbilityScoresComponent,
-    ProficienciesComponent
+    ProficienciesComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
