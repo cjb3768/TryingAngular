a3cddd852eb5ecfd83b96ae817ef3cf55c7c3417
Cullen Brown <cjb3768@gmail.com>
2019-04-25 13:55:42 -0500

    Created SkillList class for storing and displaying skill modifiers

---
+++ char-sheet-gen/src/app/skill-list.ts
@@ -0,0 +1,20 @@
+export class SkillList {
+  acrobatics: number;
+  animalHandling: number;
+  arcana: number;
+  athletics: number;
+  deception: number;
+  history: number;
+  insight: number;
+  intimidation: number;
+  investigation: number;
+  medicine: number;
+  nature: number;
+  perception: number;
+  performance: number;
+  persuasion: number;
+  religion: number;
+  sleightOfHand: number;
+  stealth: number;
+  survival: number;
+}
