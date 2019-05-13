18e9de55d0136e869f920832c6d550fc9de0dd96
Cullen Brown <cjb3768@gmail.com>
2019-05-13 15:09:39 -0500

    Copying Event Subscription Skeleton from SkillsComponent Need to listen to some of the same events needed in SkillsComponent; might as well keep the same skeleton.

--- char-sheet-gen/src/app/saving-throws/saving-throws.component.ts
+++ char-sheet-gen/src/app/saving-throws/saving-throws.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AbilityScoresService } from '../ability-scores.service';
+import { ProficienciesService } from '../proficiencies.service';
+
 @Component({
   selector: 'app-saving-throws',
   templateUrl: './saving-throws.component.html',
@@ -7,7 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SavingThrowsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private abilityScoresService: AbilityScoresService, private proficienciesService: ProficienciesService) {
+    //abilityScoresService subscriptions
+    abilityScoresService.onUpdateEvent.subscribe(
+      (adjustedScore) => {
+        //console.log(`Modifier for "${adjustedScore}" changed. Updating relevant skills.`);
+        //this.updateSkillModifiers(adjustedScore);
+      }
+    );
+
+    //proficienciesService subscriptions
+    proficienciesService.addProficiencyEvent.subscribe(
+      (proficiencyContext) => {
+        /*
+        if (proficiencyContext.type == "skills"){
+          //verify that the skill in question exists
+          if (this.skills[proficiencyContext.name]){
+            console.log(`Updating interface to match added proficiency for "${proficiencyContext.name}"`);
+            //set value of matching proficiency checkbox
+            this.proficiencyControls.controls[proficiencyContext.name].setValue(true);
+            //enable matching expertise checkbox
+            this.expertiseControls.controls[proficiencyContext.name].enable();
+            //update modifier
+            this.skills[proficiencyContext.name].modifier = this.calculateSkillModifier(proficiencyContext.name, this.skills[proficiencyContext.name].ability);
+          }
+        }
+        else{
+          console.log(`Encountered unexpected proficiency type "${proficiencyContext.type}"`);
+        }
+        */
+      }
+    );
+
+    proficienciesService.removeProficiencyEvent.subscribe(
+      (proficiencyContext) => {
+        /*
+        if (proficiencyContext.type == "skills"){
+          //verify that the skill in question exists
+          if (this.skills[proficiencyContext.name]){
+            console.log(`Updating interface to match removed proficiency for "${proficiencyContext.name}"`);
+            //set value of matching proficiency checkbox
+            this.proficiencyControls.controls[proficiencyContext.name].setValue(false);
+            //disable matching expertise checkbox
+            this.expertiseControls.controls[proficiencyContext.name].disable();
+            //update modifier
+            this.skills[proficiencyContext.name].modifier = this.calculateSkillModifier(proficiencyContext.name, this.skills[proficiencyContext.name].ability);
+
+          }
+        }
+        else{
+          console.log(`Encountered unexpected proficiency type "${proficiencyContext.type}"`);
+        }
+        */
+      }
+    );
+  }
 
   ngOnInit() {
   }
